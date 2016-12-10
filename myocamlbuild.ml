7c73e1e8697cdf6a540e8157b37e56f4f758f68b
Xavier Guérin <github@applepine.org>
2016-12-10 17:15:59 -0500

    Remove unused dependencies

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -7,8 +7,7 @@ let () = dispatch (fun phase ->
       (Findlib.query "ppx_deriving").Findlib.location ^ "/" ^ deriver
     in
     flag ["ocaml"; "compile"; "use_hardcaml"] &
-      S[A"-ppx"; A"ocamlfind ppx_import/ppx_import";
-        A"-ppx"; A("ocamlfind ppx_deriving/ppx_deriving "^
+      S[A"-ppx"; A("ocamlfind ppx_deriving/ppx_deriving "^
                    "src/ppx_deriving_hardcaml.cma "^
                    (ppx_deriving_component "ppx_deriving_show.cma"));
         A"-I"; A(ppx_deriving_component "")];
